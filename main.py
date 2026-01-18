f5bcd7287ed0daff4b8d4b4ac83ffd66ce9bee59
nemoirisgurl <noppanart5777@gmail.com>
2026-01-18 15:40:01 +0700

    Add tabulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sqlite3
 import re
-import tabulate
 
 
 DB_NAME = "finances.db"
@@ -47,8 +46,8 @@ def view_transactions(transaction_type=None):
                 cur = con.execute("SELECT * FROM transactions")
             transactions = cur.fetchall()
             if transactions:
-                headers = ["ID", "Transaction Name", "Transaction Type", "Amount", "Transaction Date"]
-                print(tabulate.tabulate(transactions, headers, tablefmt="grid"))
+                for row in transactions:
+                    print(row)
             else:
                 print("No transactions found.")
         except sqlite3.OperationalError as e:
