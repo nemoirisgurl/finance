d3ed0df6b9118a06bcc10fcfbdc11ac939952c71
nemoirisgurl <noppanart5777@gmail.com>
2026-01-26 02:46:57 +0700

    Revert "Simplify date valid"
    This reverts commit fe9aa9b05d44f6e2bf8bb6ad553d4fadb210c443.

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,11 +1,37 @@
-from datetime import datetime
+import re
+
+DATE_REGEX = re.compile(r"^\d{4}-\d{2}-\d{2}$")  # YYYY-MM-DD format
 
 
 def is_valid_date(date):
-    try:
-        return datetime.strptime(date, "%Y-%m-%d")
-    except ValueError:
+    if not date or not DATE_REGEX.match(date):
         return False
+    date_parts = date.split("-")
+    year, month, day = map(int, date_parts)
+    if not (1 <= month <= 12):
+        return False
+    else:
+        month_days = [
+            31,
+            29 if is_leap_year(year) else 28,
+            31,
+            30,
+            31,
+            30,
+            31,
+            31,
+            30,
+            31,
+            30,
+            31,
+        ]
+        if not (1 <= day <= month_days[month - 1]):
+            return False
+    return True
+
+
+def is_leap_year(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 
 def calc_interest(
