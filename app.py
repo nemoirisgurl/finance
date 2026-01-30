a27dd18f3b4cf98c1c1b951c3c2a232541ddf21f
nemoirisgurl <noppanart5777@gmail.com>
2026-01-30 22:11:31 +0700

    Initialize database

---
+++ app.py
@@ -0,0 +1,47 @@
+import database.database as db
+import helpers.helpers as hlp
+import sys
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QTableWidget,
+    QTableWidgetItem,
+    QVBoxLayout,
+    QWidget,
+    QMessageBox,
+    QPushButton,
+    QSizePolicy,
+    QLabel,
+    QMessageBox,
+)
+from PyQt6.QtCore import Qt
+
+
+class FinanceManager(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Finance Manager")
+        self.resize(600, 400)
+
+        center_widget = QWidget()
+        self.setCentralWidget(center_widget)
+
+        self.layout = QVBoxLayout()
+        center_widget.setLayout(self.layout)
+
+        title = QLabel("Finance Manager")
+        self.layout.addWidget(title)
+        init_button = QPushButton("Initialize Database", self)
+        self.layout.addWidget(init_button)
+        init_button.clicked.connect(self.init_db)
+
+    def init_db(self):
+        db.init_db()
+        QMessageBox.information(self, "Success", "Database initialized successfully.")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = FinanceManager()
+    window.show()
+    sys.exit(app.exec())
